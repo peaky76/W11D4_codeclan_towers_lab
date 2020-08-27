c48972f24317ade1a2a75bf3e8470c6865a00940
peaky76 <robertjamespeacock@gmail.com>
2020-08-27 11:37:08 +0100

    bedroom cannot exceed capacity

--- src/test/java/BedroomTest.java
+++ src/test/java/BedroomTest.java
@@ -1,3 +1,4 @@
+import Hotel.Guest;
 import Hotel.Rooms.Bedroom;
 import Hotel.Rooms.RoomType;
 import org.junit.Before;
@@ -8,10 +9,16 @@ import static org.junit.Assert.assertEquals;
 public class BedroomTest {
 
     Bedroom bedroom;
+    Guest guest1;
+    Guest guest2;
+    Guest guest3;
 
     @Before
     public void before() {
         bedroom = new Bedroom(101, RoomType.DOUBLE);
+        guest1 = new Guest("Fred Bloggs");
+        guest2 = new Guest("Jane Doe");
+        guest3 = new Guest("John Doe");
     }
 
     @Test
@@ -27,6 +34,20 @@ public class BedroomTest {
     @Test
     public void canGetRoomType() {
         assertEquals(RoomType.DOUBLE, bedroom.getRoomType());
+    }
+
+    @Test
+    public void canAddGuest() {
+        bedroom.addGuest(guest1);
+        assertEquals(1, bedroom.getGuests().size());
+    }
+
+    @Test
+    public void cannotExceedCapacity() {
+        bedroom.addGuest(guest1);
+        bedroom.addGuest(guest2);
+        bedroom.addGuest(guest3);
+        assertEquals(2, bedroom.getGuests().size());
     }
 
 }
